b49dc29fde7d5c345a1f756bd85d44798f3c09f0
YvesYvesY <84990682+YvesYvesY@users.noreply.github.com>
2025-06-20 23:08:13 +0800

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Welcome to Home Page</h1>
+      <p>This is the homepage of MyComplicatedReply.</p>
+    </div>
+  );
+}
