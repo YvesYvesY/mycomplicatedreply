84d81e186efa9810f3f64780492c464deb17ca10
YvesYvesY <84990682+YvesYvesY@users.noreply.github.com>
2025-06-20 23:09:08 +0800

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,1 +1,8 @@
-src
+export default function Profile() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Profile Page</h1>
+      <p>This is your profile page.</p>
+    </div>
+  );
+}
