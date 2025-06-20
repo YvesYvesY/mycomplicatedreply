009333884ca94375a9333e68aacc3aeaae6ca692
YvesYvesY <84990682+YvesYvesY@users.noreply.github.com>
2025-06-20 23:08:48 +0800

    Create Chat.jsx

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,8 @@
+export default function Chat() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Chat Page</h1>
+      <p>This is where the chat interface will go.</p>
+    </div>
+  );
+}
